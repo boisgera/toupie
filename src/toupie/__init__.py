a691e8c2da55714c108f6d5570e2f5aba645a190
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2025-08-18 16:05:15 +0200

    Add Python code execution

--- src/toupie/__init__.py
+++ src/toupie/__init__.py
@@ -1,5 +1,7 @@
+import ast
+import io
 import logging
-import time
+from contextlib import redirect_stdout
 
 from flask import Flask, request
 import waitress
@@ -10,9 +12,16 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
 def handler():
-    name = request.data.decode("utf-8").strip()
-    time.sleep(10.0)
-    return f"Hello {name}\n"
+    code = request.data.decode("utf-8").strip()
+    try:
+        output_stream = io.StringIO()
+        with redirect_stdout(output_stream):
+            ast.parse(code, mode="exec")
+            exec(code, globals())
+        output = output_stream.getvalue()
+    except Exception as error:
+        output = f"{type(error).__name__}: {error}"
+    return output
 
 def main():
     waitress.serve(app, host="127.0.0.1", port=8000, threads=1)

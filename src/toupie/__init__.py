459cac0dd4dd8e8efb8ec10ea82ccb7736581982
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2025-08-18 15:22:30 +0200

    Update layout

--- src/toupie/__init__.py
+++ src/toupie/__init__.py
@@ -9,11 +9,10 @@ logging.getLogger("waitress.queue").setLevel(logging.ERROR)
 app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
-def say_hello():
+def handler():
     name = request.data.decode("utf-8").strip()
     time.sleep(10.0)
     return f"Hello {name}\n"
 
-if __name__ == "__main__":
+def main():
     waitress.serve(app, host="127.0.0.1", port=8000, threads=1)
-
